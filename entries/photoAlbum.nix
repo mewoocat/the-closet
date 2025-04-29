90b88f27ea18118b2919a40b58e4ad6e62c3e90b
mewoocat <azure.horizon.4@gmail.com>
2025-04-28 19:27:34 -0500

    Added photos

--- entries/photoAlbum.nix
+++ entries/photoAlbum.nix
@@ -7,9 +7,9 @@
   # Note that these paths are relative to the result
   photos = [
     "/photos/car.jpg"
-    "/photos/car.jpg"
-    "/photos/car.jpg"
-    "/photos/car.jpg"
-    "/photos/car.jpg"
+    "/photos/gb-kar.png"
+    "/photos/gb-kar-2.png"
+    "/photos/gb-green.png"
+    "/photos/gb-abb.png"
   ];
 }
